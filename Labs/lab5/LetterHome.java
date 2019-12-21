5a64b3f5dac0c700dfc21a7ea35cb030d18edaa6
Alex Davila <44888562+davilaha@users.noreply.github.com>
2019-12-20 17:34:36 -0800

    Add files via upload

--- Labs/lab5/LetterHome.java
+++ Labs/lab5/LetterHome.java
@@ -1,1 +1,121 @@
+//---------------------------------------------------------------------------------------
+//
+// LetterHome.java  
+// A Davila   
+// CS 11, Tantalo                                
+//
+//
+// 1) error found at line 125 || (sentenceCode >= MAX_CODE) not valid because
+// sentence code was asking for and error output line for nunbers of 5 inclusive
+// and above. Where it is supposed to be numbers only bigger than 5(not inclusive).
+//
+// 2) error found at line 191. if(m == 1)
+//         //word = "great";
+//      //if(m == 2);        //this line is 191 and it has an unecessary semicolon, which
+//         //word = "foggy"; // causes the first if statement assigns word to great but the second
+//      //if(m == 3)         // if statement doesn't run but the word for the first if statement
+//                           //gets overided and gets a new assignement to foggy.
+//
+// 3) The last error i found was here:    }else; if( modifierCode == 3 ){
+//                                          //phrase = "money";
+//                                        there is an uneccessary colon here.
+//---------------------------------------------------------------------------------------
 
+import java.util.Scanner;
+import java.io.File;
+
+class LetterHome{
+
+   static final int MAX_CODE = 5;
+
+   public static void main(String[] args) throws Exception{
+      Scanner in = new Scanner(new File(args[0]));
+      String phrase;
+      int sentenceCode, modifierCode;
+
+      // print heading
+      System.out.println("Dear Mom and Dad:\n");
+
+      // print encoded sentences
+      while( in.hasNext() ){
+
+         // read a pair of sentence and modifier codes
+         sentenceCode = in.nextInt();
+         modifierCode = in.nextInt();
+
+         // make sure the sentenceCode is within range 
+         if( (sentenceCode < 1) || (sentenceCode > MAX_CODE) ) {
+            System.out.println(sentenceCode + " is not a valid sentence code");
+            continue;
+         }
+
+         // print the appropriate sentence with modifier
+         if( sentenceCode == 1 ){
+            if( modifierCode == 1 ){
+               phrase = "great";
+            }else if( modifierCode == 2 ){
+               phrase = "ok";
+            }else{
+               phrase = "ERROR";
+            }
+            System.out.println("My classes are going " + phrase + ".");
+
+         }else if( sentenceCode == 2){
+            phrase = weatherModifier(modifierCode);
+            System.out.println("The weather here has been " + phrase + ".");
+
+         }else if( sentenceCode == 3 ){
+            if( modifierCode == 1 ){
+               phrase = "after the quarter ends";
+            }else if( modifierCode == 2 ){
+               phrase = "in a few weeks";
+            }else if( modifierCode == 3 ){
+               phrase = "next weekend";
+            }else{
+               phrase = "ERROR";
+            }
+            System.out.println("I plan to come home for a visit " + phrase + ".");
+
+         }else if( sentenceCode == 4 ){
+            System.out.println("Do you think you could send me $" + modifierCode + "?");
+            System.out.println("I have to buy another book for one of my classes.");
+
+         }else if( sentenceCode == 5 ){
+            if( modifierCode == 1 ){
+               phrase = "cookies";
+            }else if( modifierCode == 2 ){
+               phrase = "stuff";
+            }else if( modifierCode == 3 ){
+               phrase = "money";
+            }else{
+               phrase = "ERROR";
+            }
+            System.out.println("Thanks for the " + phrase + " you sent.");
+         }
+      }
+   }
+
+
+   // weatherModifier()
+   // returns the correct word for a weather sentence
+   static String weatherModifier(int m) {
+      String word=null;
+
+      // select weather modifier
+      if(m == 1)
+         word = "great";
+      if(m == 2)
+         word = "foggy";
+      if(m == 3)
+         word = "hot";
+      if(m == 4)
+         word = "cold";
+      if(m == 5)
+         word = "variable";
+      if( m<1 || m>5)
+         word = "ERROR";
+
+      return word;
+   }
+
+}
