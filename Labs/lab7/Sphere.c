314e7ccfa11e428584da3660149ab681b5b6a69d
Alex Davila <44888562+davilaha@users.noreply.github.com>
2019-12-20 17:55:48 -0800

    Add files via upload
    My first C program

--- Labs/lab7/Sphere.c
+++ Labs/lab7/Sphere.c
@@ -1,1 +1,23 @@
+//A Davila
+//lab7
+//hdavilac
 
+     /* Sphere.c */
+#include<stdio.h>
+    
+int main(void){
+float vol, surfA;
+float rad;
+const double pi = 3.141592654;
+
+printf("Enter the radius of the sphere: ");
+
+scanf(" %f", &rad);
+vol = (4.0/3.0) * (pi) * (rad*rad*rad);
+surfA = (4*pi) * (rad*rad);
+
+printf("The volume is %f and the surface area is %f.", vol , surfA);
+
+printf("\n");
+return 0;
+}
